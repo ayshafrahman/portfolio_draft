d96d45f0fa0c1d090fcb7d533affe2bb7116a3a5
ayshafrahman <ayshafrahman@outlook.com>
2020-07-23 19:50:45 -0400

    Fixed broken links

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-global.document = new JSDOM("C:/Users/mrahman/Desktop/name_animation/index.html").window.document;
+//FAILED NAME STUFF//
+/*const jsdom = require("jsdom");
+    const { JSDOM } = jsdom;
+    global.document = new JSDOM("C:/Users/mrahman/Desktop/name_animation/index.html").window.document;
 
-const logo=document.querySelectorAll('#logo path');
-console.log(logo);
-for(let i=0; i<logo.length;i++){
-    console.log(logo[i].getTotalLength());
-    /*console.log('Letter ${i} is ${logo[i].getTotalLength()}')*/
-}
+    const logo=document.querySelectorAll('#logo path');
+    console.log(logo);
+    for(let i=0; i<logo.length;i++){
+        console.log(logo[i].getTotalLength());
+        console.log('Letter ${i} is ${logo[i].getTotalLength()}')
+    }*/
 
+console.log("hi");
